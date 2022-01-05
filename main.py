9ad7812c2b04a0a0fdf5d9715ed246afe76c3f98
claws85 <christopherlaws85@gmail.com>
2022-01-05 15:44:21 +0000

    switch to email messaging, new event details

--- main.py
+++ main.py
@@ -4,22 +4,32 @@
 import datetime
 import logging
 import requests
+import smtplib
+import ssl
 
-from config import DATES, TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
+from local_config import (
+    ACCOUNT,
+    CHRIS_EMAIL,
+    DATES,
+    MAIL_SERVER,
+    MESSAGE_TEMPLATE,
+    PASSWORD,
+    PORT,
+    TELEGRAM_BOT_TOKEN,
+    TELEGRAM_CHAT_ID
+)
 
 logging.basicConfig(filename='event_reminder.log', level=logging.INFO)
 
-telegram_url = 'https://api.telegram.org/bot{}/'.format(TELEGRAM_BOT_TOKEN)
+#telegram_url = 'https://api.telegram.org/bot{}/'.format(TELEGRAM_BOT_TOKEN)
 
 time_delta = 14
-
-message_template = "The following event(s) will occur {} days from today!\n{}"
 
 
 def main():
     today = datetime.date.today()
     event_list = ''
-    event_template = '\n{}: on {} \nAddress: {}\n'
+    event_template = '\n{} on {} \nAddress: {}\n'
     try:
         for k, v in DATES.items():
             if future_date(today, time_delta) == k:
@@ -27,19 +37,31 @@ def main():
                 event_list = event_list + new_event
 
         if event_list:
+            # Create secure SSL context
+            context = ssl.create_default_context()
+
+            # send email to recipients
+            with smtplib.SMTP_SSL(MAIL_SERVER, PORT, context=context) as server:
+                server.login(ACCOUNT, PASSWORD)
+                email_sent_result = server.sendmail(
+                        ACCOUNT,
+                        CHRIS_EMAIL,
+                        MESSAGE_TEMPLATE.format(time_delta, event_list)
+                    )
+
             # call method to send notifications
-            status = send_text(message_template.format(time_delta, event_list))
+            # status = send_text(message_template.format(time_delta, event_list))
 
             dt = datetime.datetime.now().strftime("%x %X")
-            if status == 200:
-                logging.info(
-                    "Text sent to group successfully at {}.\n".format(dt)
-                )
-            if str(status).startswith('4'):
-                logging.error(
-                    "Error detected when sending message at {}."
-                    "Status code {}.\n".format(dt, status)
-                )
+            if not email_sent_result:
+                 logging.info(
+                     "Email sent to recipients successfully at {}.\n".format(dt)
+                 )
+            # if str(status).startswith('4'):
+            #     logging.error(
+            #         "Error detected when sending message at {}."
+            #         "Status code {}.\n".format(dt, status)
+            #     )
 
     except Exception as e:
         dt = datetime.datetime.now().strftime("%x %X")
@@ -47,21 +69,21 @@ def main():
             "The following error was encountered at {}:"
             "\n{}".format(dt, e)
         )
-        send_text(
-            "An exception was encountered during the running of the "
-            "event_reminder app. Check the logs :)"
-        )
+        # send_text(
+        #     "An exception was encountered during the running of the "
+        #     "event_reminder app. Check the logs :)"
+        # )
 
 
-def send_text(text):
-    """Sends some text via a bot to the chosen group. Returns
-    the request status_code for validating"""
-    url = telegram_url + 'sendMessage'
-    data = {'chat_id': TELEGRAM_CHAT_ID,
-            'text': text}
-    r = requests.post(url, data=data)
-
-    return r.status_code
+# def send_text(text):
+#     """Sends some text via a bot to the chosen group. Returns
+#     the request status_code for validating"""
+#     url = telegram_url + 'sendMessage'
+#     data = {'chat_id': TELEGRAM_CHAT_ID,
+#             'text': text}
+#     r = requests.post(url, data=data)
+#
+#     return r.status_code
 
 
 def future_date(date_today, day_delta):
